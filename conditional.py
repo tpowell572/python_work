23037bc2ade4fcb20254832bcbd26cddbfaded9f
Thomas Powell <thomasepowell@outlook.com>
2017-09-28 21:26:10 -0400

    Adding conditional example one

---
+++ conditional.py
@@ -0,0 +1,17 @@
+car='subaru'
+print("\nIs car == 'subaru'?, I predict yes")
+print(car == 'subaru')
+
+car='ford'
+print("\nIs car == 'subaru'?, I predict no")
+print(car == 'subaru')
+
+car='Ford'
+print("\nIs car == 'ford'")
+print(car == 'ford')
+print("\nIs car.title() == 'Ford' ")
+print(car.title() == 'Ford' )
+print("\nIs car.lower() == 'ford' ")
+print(car.lower() == 'ford' )
+print("\nIs car != 'Subaru' ")
+print(car != 'Subaru' )
